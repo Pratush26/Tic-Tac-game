366bb559b06b42f7b45955e218b88963f610386e
root <root@localhost>
2024-08-07 20:12:59 +0600

    auto publish 1723039979388

--- js/main.js
+++ js/main.js
@@ -30,9 +30,28 @@ btn.forEach((b)=>{
       display.innerHTML = "Player O's turn";
     }
     b.disabled = true;
+    check()
   })
 })
-function rt(){
+function win(x){
+  display.innerHTML = `Congratulations,the winner is ${x}`;
+  btn.forEach((d)=>{
+    d.disabled = true;
+  })
+}
+function check(){
+  for(let pattern of winningCombinations){
+   let x = btn[pattern[0]].innerText
+   let y = btn[pattern[1]].innerText
+   let z = btn[pattern[2]].innerText
+   console.log (x,y,z)
+   if(x!="" && y!="" && z!="" && x==y && y==z){
+     win(x)
+   }
+  }
+}
+
+function restart(){
   btn.forEach((c)=>{
     c.innerHTML = "";
     c.disabled = false;
@@ -43,7 +62,7 @@ function rt(){
 let reset = document.querySelector('.reset')
 reset.addEventListener('pointerdown',()=>{
   reset.style.backgroundColor = "darkred";
-  rt();
+  restart();
 })
 reset.addEventListener('pointerup',()=>{
   reset.style.backgroundColor = "crimson";
