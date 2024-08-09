a86b02ed9e371971482e266cbfc5a4698e981c45
root <root@localhost>
2024-08-09 11:42:04 +0600

    auto publish 1723182123932

--- js/main.js
+++ js/main.js
@@ -44,9 +44,23 @@ function check(){
   for(let pattern of winningCombinations){
    let x = btn[pattern[0]].innerText
    let y = btn[pattern[1]].innerText
-   let z = btn[pattern[2]].innerText 
+   let z = btn[pattern[2]].innerText
    if(x!="" && y!="" && z!="" && x==y && y==z){
      win(x);
+     var line = document.querySelector('span')
+     var px = pattern[0]
+     var py = pattern[1]
+     var pz = pattern[2]
+     if(py==4){
+       let an = (px+1)*45
+       line.style.cssText = 'bottom:50%; transform:rotate('+an+'deg);'
+     }
+     else if(px==0){
+       line.style.cssText = pz==6?'transform:rotate(90deg);left:-13vw;top:50%':'top:6vw'
+     }
+     else if(pz==8){
+       line.style.cssText = px==2?'transform:rotate(90deg);right:-13vw;bottom:50%':'bottom:6vw'
+     }
      return;
    }
    else if(draw()){
@@ -54,11 +68,13 @@ function check(){
    }
   }
 }
-
+//reset....
 function restart(){
   btn.forEach((c)=>{
     c.innerHTML = "";
     c.disabled = false;
+    var line = document.querySelector('span')
+    line.style.cssText = 'bottom:13vw; transform:rotate(0deg);'
   })
   display.innerHTML = "Player O's turn";
 }
